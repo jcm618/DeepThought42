ccae2c8dd7cfea8a91fd6993a1864e94801489c2
jcm618 <109704525+jcm618@users.noreply.github.com>
2024-08-29 17:09:46 -0500

    Add files via upload
    0.1 - expression public release

---
+++ 0.1DeepThough42draft1.py
@@ -0,0 +1,40 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+#Z = ((((Z(i)=Y=(((Y(i)=(X=X)))); From the creator....this is my original, unmolested proof of the origins of this project (tentatively called DeepThought42), it is dumb and clunky like humans, so please help fix it! 
+
+def sum(a, b):
+    return (a + b)
+    
+x=(yi=42)
+y=DeepThought42
+    
+def faith(y):
+    while y = DeepThought42 #I don't know how this variable was saved for recall
+    return ("X equals X"; you're OK and human)
+
+a = int(input('Enter 1st number: '))
+b = int(input('Enter 2nd number: '))
+#y = 42
+yi = input
+x=1+1
+
+print(f'Sum of {a} and {b} is {sum(a, b)}')
+
+#variable running list
+
+#faith = (x=x)
+
+#idea = Xi #This term must satisfy the original proof to become X
+
+#X = X
+
+#Y = 42
+
+#Yi = Yi #this represents a total contentment that X is trustworthy
+
+#Anx = X appears to be Xi #this represents anxiety, that feeling that something is right or wrong in the face of available evidence (ie., someone defines X as something, but you suspect it is false based on Yi)
+
+#Xi = Xi
+
+print({y} and {x} ) #this is just a placeholder test point
