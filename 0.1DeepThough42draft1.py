4353805bbf4fadafee8ce426e9aca08d5d82efab
jcm618 <jcm618@gmail.com>
2024-09-02 09:41:49 -0500

    Update 0.1DeepThough42draft1.py

--- 0.1DeepThough42draft1.py
+++ 0.1DeepThough42draft1.py
@@ -1,29 +1,29 @@
+#Welcome to ProjectDeepThought42 - This is a work in progress. I am a non-functional program and mathematical expression written by a 6th grade ape. I am very dumb, but logical.
 
-# Online Python - IDE, Editor, Compiler, Interpreter
+#Version 0.1: Z = ((((UUS(i)=Y=(((Y(i)=(X=X)))); From the creator....this is my original, unmolested proof of the origins of this project (tentatively called DeepThought42), it is dumb and clunky like humans, so please help fix it! 
 
-#Z = ((((UUS(i)=Y=(((Y(i)=(X=X)))); From the creator....this is my original, unmolested proof of the origins of this project (tentatively called DeepThought42), it is dumb and clunky like humans, so please help fix it! 
-#Version 0.2 test commit
-#UUS(i)=(Y(i)=(X=X)))
+#Version 0.2:  UUS(i)=(Y(i)=(X=X))) - Just a working refinement and thoughts on how to build useable logic. 
+
 UUS = False #Universally Unknown System; fully unproved and only theorized by lower systems
 
-Y = False "42" # This system, this program's "faith" for UUS - arbitrary assignment. Always an illogical when combined with any idea or X. This system would be nested below humans in most circumstances
+Y = False # "42" # This program's "faith" - arbitrary assignment. Always illogical when combined with any X. 
 
-Yi=False #middle system faith, unproveable and can occur anywhere between first and last system. Yi can be different for every individual system. Everyone is a Yi unless they can prove Y
+Yi = "DeepThought42" #middle system faith, unproveable and can occur anywhere between first and last system. Yi can be different for every individual system. Everyone is a Yi unless they can prove Y
 
-X=True #majority accepted known unknowns (repeatable, observable, known, agreed upon), used universally until no longer meets criteria
+X = True #majority accepted known unknowns (repeatable, observable, known, agreed upon), used universally until no longer meets criteria
 
-Xi=False #not majority accepted, may be repeatable, observable, or known within some systems. remains illogical or irrational until meets criteria for X
+Xi = False #not majority accepted, may be repeatable, observable, or known within some systems. remains illogical or irrational until meets criteria for X
    
-X = lambda Yi=42: Yi
-Y="DeepThought42"
+#X = lambda Yi=42: Yi
+#Y = "DeepThought42"
     
-def faith(Y):
-    while Y == "DeepThought42": #I don't know how this variable was saved for recall
-        return ("'X equals X'; you're OK and human")
+#def faith(Y):
+#    while Y == "DeepThought42": #I don't know how this variable was saved for recall
+#        return ("'X equals X'; you're OK and human")
 
 #y = 42
-yi = input
-x=1+1
+#yi = input
+#x=1+1
 
 #variable running list
 
